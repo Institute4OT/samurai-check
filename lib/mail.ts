eefb2e29d71463545435f16aea6eb27641f34959
Institute4OT <ourdx.mtg@gmail.com>
2025-09-25 20:55:02 +0900

    fixed build Error:　sendMail

--- lib/mail.ts
+++ lib/mail.ts
@@ -9,14 +9,23 @@ type SendMailParams = {
   subject: string;
   html: string;
   text?: string; // ✅ 追加！
+  replyTo?: string; // ✅追加！
+  tagId?: string;   // ✅追加！
 };
 
-export async function sendMail({ to, subject, html, text }: SendMailParams) {
+export async function sendMail({ to, subject, html, text, replyTo, tagId}: SendMailParams) {
   await resend.emails.send({
     from: '武将タイプ診断 <report@ourdx-mtg.com>',
     to,
     subject,
     html,
     text, // ✅ 追加！
+    replyTo: replyTo,  // ✅ resend API用プロパティ名
+    tags: tagId ? [{ name: "tagId", value: tagId }] : undefined, // ✅ tagsの形式に変換
   });
 }
+
+export async function sendReportMail(params: SendMailParams) {
+  return await sendMail(params); // 既存のsendMailを呼ぶだけでOK
+}
+
